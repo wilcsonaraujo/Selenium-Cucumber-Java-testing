00052cea0ae1c294fe02b954477ef1654a9b4e1c
Wilcson Araújo <32008067+wilcsonaraujo@users.noreply.github.com>
2023-05-08 10:13:04 -0300

    Update RunCucumberTest.java

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -10,9 +10,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {}, //Coloca configurações de report de teste
+        plugin = {}, 
         features = "src/test/resources/features",
-        tags = {"~@ignore"}, //Roda tudo que é diferente de ignore
+        tags = {"~@ignore"}, 
         glue = {"steps"}
 )
 
